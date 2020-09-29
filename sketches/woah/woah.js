609b6987f9d3319af240acd18a00e008b820fcb3
Saif <68385788+safekhawaja@users.noreply.github.com>
2020-09-29 14:55:45 +0400

    woah

--- sketches/woah/woah.js
+++ sketches/woah/woah.js
@@ -1,2 +1,20 @@
-https://p5js.org/examples/drawing-patterns.html but with some animated guy on the cursor
-//change from chill to woahhhh
+function preload() {
+    font = loadFont('/Users/saif/Documents/GitHub/js_art/sketches/woah/SuisseIntl.otf');
+  }
+
+function setup() {
+    createCanvas(710, 400);
+    background(102);
+  }
+  
+  function draw() {
+    variableWoah(mouseX, mouseY, pmouseX, pmouseY);
+  }
+  
+  function variableWoah(x, y, px, py) {
+    let speed = abs(x - px) + abs(y - py);
+    textFont(font);
+    fill(0);
+    fontsize(speed);
+    text('woah', x, y);
+  }
