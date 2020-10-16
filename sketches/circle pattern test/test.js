0337029acc1b72da1f7c82e785b46681abf8d55d
Saif <68385788+safekhawaja@users.noreply.github.com>
2020-10-16 13:58:09 +0400

    Update test.js

--- sketches/circle pattern test/test.js
+++ sketches/circle pattern test/test.js
@@ -1,15 +1,27 @@
+var y_fillings = [];
+
 function setup() {
     createCanvas(400, 400);
     x_filling = map(mouseX, 0, 400, 255, 0);
   }
   
-  function draw() {
-    if (mouseIsPressed || (mouseX > height / 2)) {
-      y_filling = random(0, 255);
-      fill(y_filling);
-      } else {
-        fill(x_filling);
-      }
-    noStroke();
-    ellipse(mouseX, mouseY, 80, 80);
-  }
+function draw() {
+    if (mouseY > height / 2) {
+        for (var i = 0; i< 3; i++) {
+            y_fillings[i] = random(255);
+        }
+
+        fill(y_fillings[0], y_fillings[1], y_fillings[2]);
+    } 
+  
+    else if (mouseIsPressed) {
+        fill(255);
+    }
+    
+    else {
+        fill(x_filling, random(255), random(255));
+    }
+    
+    // noStroke();
+    ellipse(mouseX, mouseY, 40, 40);
+}
